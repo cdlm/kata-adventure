39b347bd1647c25538c8651905333b8a4888576e
Damien Pollet <damien.pollet@gmail.com>
2012-11-24 03:52:41 +0100

    Clarify artifact test

--- src/adventure/tests/ArtifactTest.java
+++ src/adventure/tests/ArtifactTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 import static org.junit.matchers.JUnitMatchers.hasItem;
@@ -38,27 +39,34 @@ public class ArtifactTest {
 
     @Test
     public void test_artifactsPresent() {
-        assertThat(lab.initialLocation().availableArtifacts(), hasItem(key));
+        assertThat(adventure.inventory(), not(hasItem(key)));
+
+        assertTrue(adventure.isArtifactAvailable(key));
+        assertFalse(adventure.isArtifactAvailable(cabinet));
+
         assertThat(lab.anotherLocation().availableArtifacts(), hasItem(cabinet));
+        assertThat(lab.anotherLocation().availableArtifacts(), not(hasItem(key)));
     }
 
     @Test
     public void test_matchesKey() {
         List<Artifact> artifacts = lab.initialLocation().findArtifacts(new String[]{"key"});
         assertThat(artifacts, hasItem(key));
+        assertThat(artifacts, not(hasItem(cabinet)));
     }
 
     @Test
     public void test_takeKey() {
-        assertThat(adventure.environmentArtifacts(), hasItem(key));
-
         narrator.react("take key");
         assertThat(adventure.environmentArtifacts(), not(hasItem(key)));
         assertThat(adventure.inventory(), hasItem(key));
-
-        narrator.react("go door");
-        assertThat(adventure.environmentArtifacts(), hasItem(cabinet));
-        assertTrue(adventure.isArtifactAvailable(key));
     }
 
+    @Test
+    public void test_transportKey() {
+        narrator.react("take key");
+        narrator.react("go door");
+        assertTrue(adventure.isArtifactAvailable(key));
+        assertTrue(adventure.isArtifactAvailable(cabinet));
+    }
 }
